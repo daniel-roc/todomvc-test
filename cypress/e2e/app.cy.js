18d367c6c3fdaebea641d4d8be57194855aff8ff
Daniel Rocha <daniel.rocha@outlook.com.br>
2022-06-01 15:59:03 -0300

    first test case

---
+++ cypress/e2e/app.cy.js
@@ -0,0 +1,9 @@
+describe("React TodoMVC", () => {
+
+    it("adds a single todo", () => {
+        cy.visit("http://localhost:8888")
+        cy.get(".new-todo").type("Buy Milk{enter}")
+        cy.get(".todo-list li").should("have.length", 1)
+    })
+
+})
