6776dd7396b3ac3350af54d2dc4d81bc7386b2fa
Daniel Rocha <daniel.rocha@outlook.com.br>
2022-06-01 16:37:47 -0300

    setting up data before each test

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,9 +1,28 @@
 describe("React TodoMVC", () => {
 
-    it("adds a single todo", () => {
+    const TODO_ITEM_ONE = 'Buy Milk'
+    const TODO_ITEM_TWO = 'Pay Rent'
+    const TODO_ITEM_THREE = 'Pickup Dry Cleaning'
+
+    beforeEach(() => {
         cy.visit("http://localhost:8888")
+    })
+
+    it("adds a single todo", () => {
+        
         cy.get(".new-todo").type("Buy Milk{enter}")
         cy.get(".todo-list li").should("have.length", 1)
+        cy.get(".todo-list li").eq(0).find("label").should("contain", TODO_ITEM_ONE)
+
+        
+    })
+
+    it("adds three todos", () => {
+        cy.get(".new-todo")
+            .type(`${TODO_ITEM_ONE}{enter}`)
+            .type(`${TODO_ITEM_TWO}{enter}`)
+            .type(`${TODO_ITEM_THREE}{enter}`)
+        cy.get(".todo-list li").should("have.length", 3)
     })
 
 })
